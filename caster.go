7e9261ca165927e465c611d5d0c9fe938c2a34de
tomocy <tomocy.dev@gmail.com>
2018-12-26 19:15:00 +0900

    make Caster interface and add ExtendAll

--- caster.go
+++ caster.go
@@ -6,24 +6,40 @@ import (
 	"io"
 )
 
-type Caster struct {
+type Caster interface {
+	ExtendAll(map[string][]string) error
+	Extend(key string, fnames ...string) error
+	Cast(w io.Writer, key string, data interface{}) error
+}
+
+type caster struct {
 	tmpls      map[string]*template.Template
 	layoutTmpl *template.Template
 }
 
-func New(layouts ...string) (*Caster, error) {
+func New(layouts ...string) (Caster, error) {
 	layoutTmpl, err := template.ParseFiles(layouts...)
 	if err != nil {
 		return nil, fmt.Errorf("faild to create new caster: %s\n", err)
 	}
 
-	return &Caster{
+	return &caster{
 		tmpls:      make(map[string]*template.Template),
 		layoutTmpl: layoutTmpl,
 	}, nil
 }
 
-func (c *Caster) Extend(key string, fnames ...string) error {
+func (c *caster) ExtendAll(m map[string][]string) error {
+	for key, fnames := range m {
+		if err := c.Extend(key, fnames...); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *caster) Extend(key string, fnames ...string) error {
 	tmpl, err := c.layoutTmpl.Clone()
 	if err != nil {
 		return fmt.Errorf("faild to extend template: %s\n", err)
@@ -37,7 +53,7 @@ func (c *Caster) Extend(key string, fnames ...string) error {
 	return nil
 }
 
-func (c *Caster) Cast(w io.Writer, key string, data interface{}) error {
+func (c *caster) Cast(w io.Writer, key string, data interface{}) error {
 	tmpl, ok := c.tmpls[key]
 	if !ok {
 		return fmt.Errorf("faild to cast template: no template whose key is %s found", key)
