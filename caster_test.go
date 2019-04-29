62ed0d6e72b4736bc8847fe02aefa9e5aacdef88
tomocy <tomocy.dev@gmail.com>
2019-04-30 05:41:54 +0900

    fix test

--- caster_test.go
+++ caster_test.go
@@ -6,101 +6,83 @@ import (
 	"fmt"
 	"html/template"
 	"io"
+	"path/filepath"
 	"strings"
 	"testing"
 )
 
 func TestCast(t *testing.T) {
-	expected := "<html><head><title>Caster test</title></head><body><header><div><h1>Header</h1></div></header><div><div><h1>Content</h1><h3>hello, world</h3></div></div><footer><div><h6>Footer</h6></div></footer></body></html>"
-	tester := &tester{
-		tset: &TemplateSet{
-			Filenames: []string{
-				"testdata/master.html",
-				"testdata/header.html",
-				"testdata/footer.html",
-			},
+	caster, err := New(&TemplateSet{
+		Filenames: []string{
+			templateFile("master.html"),
+			templateFile("header.html"),
+			templateFile("footer.html"),
 		},
-	}
-	if err := tester.extend("var", &TemplateSet{
-		Filenames: []string{"testdata/var.html"},
-	}); err != nil {
-		t.Errorf("unexpected error: %s\n", err)
-	}
-
-	b := &bytes.Buffer{}
-	if err := tester.cast(b, "var", map[string]interface{}{
-		"message": "hello, world",
-	}); err != nil {
-		t.Errorf("unexpected error: %s\n", err)
-	}
-
-	actual := trimWhitespaces(b)
-	if actual != expected {
-		t.Errorf("unexpected output: got %s, expected %s\n", actual, expected)
-	}
-}
-
-func TestCastWithDefaultFuncs(t *testing.T) {
-	expected := "<html><head><title>Caster test</title></head><body><header><div><h1>hello, tomocy</h1></div></header><div><div><h1>Content</h1><h3>hello, world</h3></div></div><footer><div><h6>Footer</h6></div></footer></body></html>"
-	tester := &tester{
-		tset: &TemplateSet{
-			Filenames: []string{
-				"testdata/master.html",
-				"testdata/header_func.html",
-				"testdata/footer.html",
-			},
-			FuncMap: template.FuncMap{
-				"greet": func(to string) string {
-					return fmt.Sprintf("hello, %s\n", to)
-				},
-			},
-		},
-	}
-
-	if err := tester.extend("var", &TemplateSet{
-		Filenames: []string{"testdata/var.html"},
-	}); err != nil {
-		t.Errorf("unexpected error: %s\n", err)
-	}
-
-	b := &bytes.Buffer{}
-	if err := tester.cast(b, "var", map[string]interface{}{
-		"to":      "tomocy",
-		"message": "hello, world",
-	}); err != nil {
-		t.Errorf("unexpected error: %s\n", err)
-	}
-
-	actual := trimWhitespaces(b)
-	if actual != expected {
-		t.Errorf("unexpected output: got %s, expected %s\n", actual, expected)
-	}
-}
-func TestCastWithFuncs(t *testing.T) {
-	expected := "<html><head><title>Caster test</title></head><body><header><div><h1>Header</h1></div></header><div><div><h1>Content</h1><h3>hello, tomocy</h3></div></div><footer><div><h6>Footer</h6></div></footer></body></html>"
-	tester := newTester()
-	if err := tester.extend("func", &TemplateSet{
-		Filenames: []string{"testdata/func.html"},
 		FuncMap: template.FuncMap{
-			"greet": func(to string) string {
-				return fmt.Sprintf("hello, %s\n", to)
+			"dear": func(to string) string {
+				return fmt.Sprintf("Dear %s,", to)
+			},
+			"sincerely": func(from string) string {
+				return fmt.Sprintf("Sincerely, %s", from)
 			},
 		},
-	}); err != nil {
-		t.Errorf("unexpected error: %s\n", err)
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %s\n", err)
 	}
 
-	b := &bytes.Buffer{}
-	if err := tester.cast(b, "func", map[string]interface{}{
-		"to": "tomocy",
+	if err := caster.Extend("test", &TemplateSet{
+		Filenames: []string{templateFile("content.html")},
 	}); err != nil {
-		t.Errorf("unexpected error: %s\n", err)
+		t.Fatalf("unexpected error: %s\n", err)
 	}
 
-	actual := trimWhitespaces(b)
-	if actual != expected {
-		t.Errorf("unexpected output: got %s, expected %s\n", actual, expected)
+	dest := new(bytes.Buffer)
+	if err := caster.Cast(dest, "test", map[string]interface{}{
+		"to":      "developers",
+		"message": "Did you sleep well last night?",
+		"from":    "tomocy",
+	}); err != nil {
+		t.Fatalf("unexpected error: %s\n", err)
 	}
+
+	actual := trimWhitespaces(dest)
+	if actual != expected() {
+		t.Errorf("got %s, expected %s\n", actual, expected())
+	}
+}
+
+func expected() string {
+	return trimWhitespaces(strings.NewReader(`
+	<html>
+
+	<head>
+		<title>Caster test</title>
+	</head>
+
+	<body>
+		<header>
+			<div>
+				<h1>Header</h1>
+				<h3>Dear developers,</h3>
+		</div>
+		</header>
+		<main>
+			<div>
+				<h1>Content</h1>
+				<h3>Did you sleep well last night?</h3>
+			</div>
+		</main>
+		<footer>
+			<div>
+				<h1>Footer</h1>
+				<h3>Sincerely, tomocy</h3>
+			</div>
+		</footer>
+	</body>
+
+	</html>
+	`))
 }
 
 func trimWhitespaces(r io.Reader) string {
@@ -118,37 +100,6 @@ func trimWhitespaces(r io.Reader) string {
 	return string(b)
 }
 
-type tester struct {
-	caster Caster
-	tset   *TemplateSet
-}
-
-func newTester() *tester {
-	return &tester{
-		tset: &TemplateSet{
-			Filenames: []string{
-				"testdata/master.html",
-				"testdata/header.html",
-				"testdata/footer.html",
-			},
-		},
-	}
-}
-
-func (t *tester) extend(key string, tset *TemplateSet) error {
-	caster, err := New(t.tset)
-	if err != nil {
-		return err
-	}
-	if err := caster.Extend(key, tset); err != nil {
-		return err
-	}
-
-	t.caster = caster
-
-	return nil
-}
-
-func (t *tester) cast(w io.Writer, key string, data interface{}) error {
-	return t.caster.Cast(w, key, data)
+func templateFile(fname string) string {
+	return filepath.Join("testdata", fname)
 }
